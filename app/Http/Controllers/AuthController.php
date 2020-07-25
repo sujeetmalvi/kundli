2cec8e8611943a19539914ad15e404e2f73ad7f7
root <root@instance-1.europe-west2-c.c.modern-environs-280506.internal>
2020-07-25 09:47:19 +0000

    added id to login api response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -191,7 +191,8 @@ class AuthController extends Controller
         $token->save();
         return response()->json(['status'=>true,
             'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
+	    'token_type' => 'Bearer',
+            'id'=> $user->id,
             'expires_at' => Carbon::parse(
                 $tokenResult->token->expires_at
             )->toDateTimeString()
