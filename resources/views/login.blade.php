8e998756f8a1a7c75a7911502acaf81229ca0be0
sujeet malvi <sujeetmalvi@gmail.com>
2020-09-12 18:18:41 +0000

    login page background update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,7 +18,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition login-page" style="background:url('{{ url('/dist/img/Nature.jpg') }}')">
 <div class="login-box">
   <div class="login-logo">
     <a href="#"><img src="{{ url('/dist/img/logo.png') }}" style="heigth:100px;width:100px;"></a><br/>
